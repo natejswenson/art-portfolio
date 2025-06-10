e7e2e71a7f33f5b4069371ada3d59785c71a461e
natejay <natejswenson@gmail.com>
2025-06-09 21:09:58 -0500

    Fix asset paths for GitHub Pages deployment

--- art-portfolio/vite.config.ts
+++ art-portfolio/vite.config.ts
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin']
-      }
-    })
-  ],
+  plugins: [react()],
+  base: '/art-portfolio/',
 })
